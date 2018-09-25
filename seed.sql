dffc6d1ef7830a7685138068235a738dc335bdc6
Ana <aparakaki17@gmail.com>
2018-09-24 22:04:11 -0700

    update seed file

--- seed.sql
+++ seed.sql
@@ -1,18 +1,18 @@
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Laptop", "Electronics", 700,  4),
-("Jeans", "Clothing", 35, 10),
-("Desk", "Furniture", 120, 5), 
-("Chair", "Furniture", 60, 9),
-("Jacket", "Clothing", 42, 5),
-("Monopoly", "Toys & Games", 29, 7),
-("Play-Doh", "Toys & Games", 18, 4),
-("Monitor", "Electronics", 198, 3),
-("Shirt", "Clothing", 24, 7),
-("Dog Food", "Pets", 19, 6);
+INSERT INTO products (product_name, department_name, price,stock_quantity, product_sales)
+VALUES ("Laptop", "Electronics", 700,  4, 37908),
+("Jeans", "Clothing", 35, 10, 8960),
+("Desk", "Furniture", 120, 5, 16080), 
+("Chair", "Furniture", 60, 9, 14460),
+("Jacket", "Clothing", 42, 5, 4368),
+("Monopoly", "Toys & Games", 29, 7, 2987),
+("Play-Doh", "Toys & Games", 18, 4, 3618),
+("Monitor", "Electronics", 198, 3, 6336),
+("Shirt", "Clothing", 24, 7, 6025),
+("Dog Food", "Pets", 19, 6, 14269);
 
 INSERT INTO departments (department_name, over_head_costs)
-VALUES ("Electronics", 38000),
-("Clothing", 47000),
-("Furniture", 83000),
-("Toys & Games", 23000),
-("Pets", 18000);
+VALUES ("Electronics", 28000),
+("Clothing", 17000),
+("Furniture", 23000),
+("Toys & Games", 3700),
+("Pets", 9700);
